d548b6fa506bce015911ac52fa35bf355d21b9e4
Wade <wbroem01@louisville.edu>
2024-10-30 19:23:06 -0500

    [fixes] removing bfield work

--- src/relativity.py
+++
@@ -1,9 +0,0 @@
-"""
-Utility functions to do Lorentz tranformations and other necessary operations
-to incorporate special relativity. 
-"""
-
-# NOTE: may end up just working this into the classes in mfield.py
-
-def calc_lorentz_transform():
-    pass
